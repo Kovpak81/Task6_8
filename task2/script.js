45701c283f5b7e90517d423906e0a24dc411e7e9
Kovpak81 <106636803+Kovpak81@users.noreply.github.com>
2022-07-11 14:20:42 +1000

    Initial commit

---
+++ task2/script.js
@@ -0,0 +1,19 @@
+const consoleLog = document.querySelector('#consoleLog');
+
+consoleLog.addEventListener('click',() => {
+    alert('Метод для вывода сообщения в веб-консоль')
+})
+
+const consoleLogExemple = document.querySelector('#consoleLogExemple');
+
+consoleLogExemple.textContent = 'Пример использования команды console.log';
+
+const alertExemple = document.querySelector('#alertExemple');
+
+alertExemple.textContent = 'Пример использования команды alert';
+
+const promptExemple = document.querySelector('#promptExemple');
+
+promptExemple.textContent = 'Пример использования команды prompt';
+
+
