d8dd867bec987f5a9d1c1e46f695c0b11621cf1b
Kovpak81 <106636803+Kovpak81@users.noreply.github.com>
2022-07-18 20:46:26 +1000

    Проверено, все работает

--- task4/script.js
+++ task4/script.js
@@ -3,6 +3,6 @@ const changeText = document.querySelector('#changeText');
 changeText.addEventListener('click', function(event) {
     event.preventDefault();
     this.textContent = prompt('Введите текст ссылки');
-})
+});
 
 
--- task6/script.js
+++ task6/script.js
@@ -5,11 +5,9 @@ let activeLight = 0;
 function changeLight() {
     circles[activeLight].className = 'circle';
     activeLight++;
-
     if (activeLight > 2) {
         activeLight = 0;
     }
-
     const currentLight = circles[activeLight];
     currentLight.classList.add(currentLight.getAttribute("color"))
 }
